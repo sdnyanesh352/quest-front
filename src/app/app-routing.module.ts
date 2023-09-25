3e7e9e45001ea354ff49ac2d3752b8f2e43b0f7f
Dnyaneshwar Vynkati Somvanshi <dnyaneshwar.s@Yash.com>
2023-09-25 20:17:09 +0530

    assignment added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
-
+import{AssignmentComponent} from './assignments/assignment/assignment.component';
+import{Forms2509dComponent} from './assignments/forms2509d/forms2509d.component'
 const routes: Routes = [
 
   {path:"",component:HomeComponent},
 
   {path:"employee",component:HomeComponent},
-  {path:"contactUs",component:ContactUsComponent}
+  {path:"contactUs",component:ContactUsComponent},
+  {path:"assignment",component:AssignmentComponent},
+  {path:"assignment01",component:Forms2509dComponent}
+
 
 ];
 @NgModule({
---
+++ src/app/assignments/forms2509d/forms2509d.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { EmployeeForm } from '../../components/model/employeeform';
+import { AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms2509d',
+  templateUrl: './forms2509d.component.html',
+  styleUrls: ['./forms2509d.component.css']
+})
+export class Forms2509dComponent {
+  emp!:EmployeeForm[];
+
+  empform!:FormGroup;
+
+  namepattern="^[a-zA-Z ]{2,15}$";
+  emailpattern="^[a-zA-Z ]+@[a-zA-Z]+\.[a-zA-Z]{2,}$";
+  isSubmitted = false;
+  City: any = ['Stable', 'Critical', 'Finished'];
+ 
+
+
+  constructor(private fb:FormBuilder)
+
+  {
+
+ 
+
+  }
+
+  ngOnInit():void{
+
+    this.empform=this.fb.group(
+
+      {
+
+        projectName:['',[Validators.required,Validators.pattern(this.namepattern)]],
+
+        email:['',[Validators.required,Validators.pattern(this.emailpattern)]],
+        cityName:['',[Validators.required]]
+
+      }
+
+    )
+
+  }
+
+  submit()
+
+  {
+
+    if(this.empform.valid)
+
+    {
+
+      console.log(this.empform.value);
+
+    }
+
+  }
+
+  changeCity(e: any) {
+    this.cityName?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+  // Access formcontrols getter
+  get cityName() {
+    return this.empform.get('cityName');
+  }
+
+
+}
