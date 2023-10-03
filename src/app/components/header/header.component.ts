0d07b06d2d33b39ffbcc5b58180895d6c5af4a66
Dnyaneshwar Vynkati Somvanshi <dnyaneshwar.s@Yash.com>
2023-10-03 19:05:11 +0530

    firebase google auth added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isLoggedIn:boolean=false;
+  constructor(private auth:AuthService){
+    this.isLoggedIn=auth.isLoggedIn;
+
+  }
 
 }
